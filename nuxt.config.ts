248e79eca8c5fe98fec98d4b93b5c2d0b1d31301
frans.suryapradipta <frans.suryapradipta@kompas.com>
2026-05-05 16:37:17 +0700

    chore: CI gitlab

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/ui'
-  ],
+  modules: ['@nuxt/eslint', '@nuxt/ui', 'nuxt-skill-hub'],
 
   devtools: {
     enabled: true
